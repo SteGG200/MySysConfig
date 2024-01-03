153340c4e82092a56df0359f933e57a3a2443ee2
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-01-03 20:23:17 +0700

    Change right prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -4,11 +4,11 @@ function fish_right_prompt
 
     set -l d (set_color brgrey)(date +%H:%M)(set_color normal)
 
-    set -q VIRTUAL_ENV_DISABLE_PROMPT
-    or set -g VIRTUAL_ENV_DISABLE_PROMPT true
-    set -q VIRTUAL_ENV
-    and set -l venv (string replace -r '.*/' '' -- "$VIRTUAL_ENV")
+    # set -q VIRTUAL_ENV_DISABLE_PROMPT
+    # or set -g VIRTUAL_ENV_DISABLE_PROMPT true
+    # set -q VIRTUAL_ENV
+    # and set -l venv (string replace -r '.*/' '' -- "$VIRTUAL_ENV")
 
     set_color normal
-    string join " " -- $venv $d
+    string join " " -- $d
 end
