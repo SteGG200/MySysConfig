4b8470bd3e62d270143639740d5bf3a653f2761e
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-02-27 23:37:07 +0700

    Add git stuff

---
+++ nvim/lua/plugins/git-stuff.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function ()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "gph", "<cmd>Gitsigns preview_hunk<cr>", {})
+		end
+	},
+}
