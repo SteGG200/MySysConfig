c8118e850bdbf6a3326968c3b8183523a7b65981
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-02-27 23:45:01 +0700

    Add keymap for navigation vim panels

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -21,3 +21,10 @@ vim.keymap.set("n", "<leader>nh", ":noh<CR>")
 
 -- Don't show the mode, since it's already in the status line
 o.showmode = false
+
+-- Navigate vim panels better
+vim.keymap.set("n", "<C-K>", "<cmd>wincmd k<cr>", {})
+vim.keymap.set("n", "<C-J>", "<cmd>wincmd j<cr>", {})
+vim.keymap.set("n", "<C-H>", "<cmd>wincmd h<cr>", {})
+vim.keymap.set("n", "<C-L>", "<cmd>wincmd l<cr>", {})
+
