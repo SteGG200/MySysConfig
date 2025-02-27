ff1d2645abe9ce934a3b470014a972f7a25339f2
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-02-27 22:43:59 +0700

    remove useless comment

--- scripts/nvim/update.ps1
+++ scripts/nvim/update.ps1
@@ -1,7 +1,4 @@
 $Source = ".\nvim\"
 $Destination = "$($env:LOCALAPPDATA)\nvim\"
-# 
-# Remove-item -LiteralPath $Destination -Recurse -Force
-# # Copy-Item -Path $Source -Destination $Destination -Recurse -Force
-# Get-Chilren 
+
 Copy-Item -Path $Source\* -Destination $Destination -Recurse -Force
