4dabd3d98b981db32b648f81245cd49d9860755d
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-02-27 22:33:24 +0700

    fix nvim update.ps1

--- scripts/nvim/update.ps1
+++ scripts/nvim/update.ps1
@@ -1,1 +1,7 @@
-Copy-Item -Path .\nvim\ -Destination $env:LOCALAPPDATA\nvim\ -Recurse -Force
+$Source = ".\nvim\"
+$Destination = "$($env:LOCALAPPDATA)\nvim\"
+# 
+# Remove-item -LiteralPath $Destination -Recurse -Force
+# # Copy-Item -Path $Source -Destination $Destination -Recurse -Force
+# Get-Chilren 
+Copy-Item -Path $Source\* -Destination $Destination -Recurse -Force
