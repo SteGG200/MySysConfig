aebf3726f6335aca9fcbc7a1b9eaa145ba882e17
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-02-28 10:54:08 +0700

    Disable keymap reveal_file in neotree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -22,7 +22,7 @@ return {
 		})
 		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left reveal_force_cwd<CR>", {})
 		vim.keymap.set("n", "<leader>b", ":Neotree close<CR>", {})
-		vim.keymap.set("n", "<F12>", ":Neotree float reveal_file=<cfile> reveal_force_cwd<cr>", {})
+		-- vim.keymap.set("n", "<F12>", ":Neotree float reveal_file=<cfile> reveal_force_cwd<cr>", {})
 		vim.keymap.set("n", "<leader>g", ":Neotree float git_status<cr>", {})
 	end,
 }
