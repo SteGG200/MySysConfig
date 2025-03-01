2e6c61dfb756bf8c5a9ecc343fd3065a8800227f
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-03-01 23:04:35 +0700

    Add autopair and indent blankline plugin

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -22,26 +22,28 @@ return {
 						runtime = {
 							-- Tell the language server which version of Lua you're using
 							-- (most likely LuaJIT in the case of Neovim)
-							version = 'LuaJIT',
+							version = "LuaJIT",
 						},
 						diagnostics = {
 							-- Get the language server to recognize the `vim` global
 							globals = {
-								'vim',
-								'require',
+								"vim",
+								"require",
 							},
 						},
 						workspace = {
-							 -- Make the server aware of Neovim runtime files
+							-- Make the server aware of Neovim runtime files
 							library = vim.api.nvim_get_runtime_file("", true),
 						},
 						telemetry = {
 							-- Do not send telemetry data containing a randomized but unique identifier
 							enable = false,
 						},
-					}
+					},
 				},
 			})
+
+			lspconfig.clangd.setup({})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
