cb15c66642be9eefa8fca0541003e3b1fa473919
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-03-09 00:07:13 +0700

    Feat: Add debugging tool for c++ on windows

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -15,7 +15,31 @@ return {
 		},
 		config = function()
 			local dap = require("dap")
+
+			dap.adapters.cppdbg = {
+				id = 'cppdbg',
+				type = 'executable',
+				command = 'C:\\Users\\ADMIN\\AppData\\Local\\nvim-data\\mason\\bin\\OpenDebugAD7.cmd',
+				options = {
+					detached = false,
+				},
+			}
+
+			dap.configurations.c = {
+				{
+					name = "Launch file",
+					type = "cppdbg",
+					request = "launch",
+					program = '${fileDirname}/${fileBasenameNoExtension}.exe',
+					cwd = '${workspaceFolder}',
+					stopAtEntry = true,
+				}
+			}
+
+			dap.configurations.cpp = dap.configurations.c
+
 			local dapui = require("dapui")
+			dapui.setup()
 
 			dap.listeners.before.attach.dapui_config = function()
 				dapui.open()
@@ -32,6 +56,13 @@ return {
 
 			vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, {})
 			vim.keymap.set("n", "<F5>", dap.continue, {})
+			vim.keymap.set("n", "<F10>", dap.step_over, {})
+			vim.keymap.set("n", "<F11>", dap.step_into, {})
+			vim.keymap.set("n", "<F12>", function ()
+				dap.terminate()
+				dapui.close()
+			end, {})
+			vim.keymap.set("n", "<C-D>", dapui.toggle, {})
 
 			-- Format breakpoint symbol
 			vim.api.nvim_set_hl(0, "red", { fg = "#f8312f" })
