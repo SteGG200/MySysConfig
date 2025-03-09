baa08d3a62a0244dc7aea153a426b8cb5324a8e8
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-03-09 07:36:26 +0700

    Feat: Handle destination in nvim powershell script

--- scripts/nvim/update.ps1
+++ scripts/nvim/update.ps1
@@ -1,4 +1,13 @@
 $Source = ".\nvim\"
 $Destination = "$($env:LOCALAPPDATA)\nvim\"
 
+if (Test-Path -Path $Destination){
+	$children = Get-ChildItem -Path $Path -Force
+	$children | Foreach-Object {
+		Remove-item -Force $_.FullName
+	}
+}else {
+	mkdir $Destination
+}
+
 Copy-Item -Path $Source\* -Destination $Destination -Recurse -Force
