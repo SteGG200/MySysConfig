bb9343592c9e7116469b4e2633564ce9a7870a82
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-03-27 12:56:12 +0700

    refactor: Change description of keymap preview hunk

--- nvim/lua/plugins/git-stuff.lua
+++ nvim/lua/plugins/git-stuff.lua
@@ -7,7 +7,7 @@ return {
 		config = function()
 			require("gitsigns").setup()
 
-			vim.keymap.set("n", "gph", "<cmd>Gitsigns preview_hunk<cr>", { silent = true, desc = "Show git diff" })
+			vim.keymap.set("n", "gph", "<cmd>Gitsigns preview_hunk<cr>", { silent = true, desc = "[P]review [H]unk (git diff)" })
 		end,
 	},
 }
