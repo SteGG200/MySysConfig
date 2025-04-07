cb92e59fa105e8613a796a246447ce8861aac548
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-04-07 21:11:42 +0700

    feat: Add sudo function for powershell

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -74,6 +74,14 @@ Set-PSReadLineOption -Colors @{
 Set-Alias -Name touch -Value New-Item
 Set-Alias -Name which -Value Get-Command
 
+function sudo {
+	param(
+		[string]$Script
+	)
+
+	Start-Process -Verb RunAs powershell.exe -Args "-executionpolicy bypass -command Set-Location \`"$PWD\`"; $Script"
+}
+
 function reboot {
 	shutdown.exe /r /t 0
 }
