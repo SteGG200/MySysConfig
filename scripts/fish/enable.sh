b060be6269d3afba852d1274ecbdf7562324e8c6
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-04-08 07:20:40 +0700

    fix: Fix fish enable script

--- scripts/fish/enable.sh
+++ scripts/fish/enable.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-FISH_DIR=$PWD/fish
+FISH_SOURCE=$PWD/fish
 FISH_DESTINATION=~/.config/fish
 FISH_BACKUP=~/.config/fish_backup
 
-if [ -d "$FISH_DIR" ]; then
+if [ -d "$FISH_DESTINATION" ]; then
 	echo "Creating backup directory for fish..."
 	mv $FISH_DESTINATION $FISH_BACKUP
 fi
 
-ln -s $FISH_DIR ~/.config/fish
+ln -s $FISH_SOURCE $FISH_DESTINATION
