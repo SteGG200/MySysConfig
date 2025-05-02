6e2c20a7d4b8fc69abe4ac1bfed211a4d0a0bc2c
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-05-02 09:38:52 +0700

    feat: Setup for dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -2,8 +2,42 @@ return {
 	"nvimdev/dashboard-nvim",
 	event = "VimEnter",
 	config = function()
+		local logo = [[
+		]]
+
+		logo = string.rep("\n", 8) .. logo .. "\n\n"
+
 		require("dashboard").setup({
 			-- config
+			theme = 'hyper',
+			hide = {
+				statusline = false,
+			},
+			config = {
+				shortcut = {
+					{ desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+					{
+						icon = ' ',
+						icon_hl = '@variable',
+						desc = 'Files',
+						group = 'Label',
+						action = 'Telescope find_files',
+						key = 'f',
+					},
+					-- {
+					-- 	desc = ' Apps',
+					-- 	group = 'DiagnosticHint',
+					-- 	action = 'Telescope app',
+					-- 	key = 'a',
+					-- },
+					-- {
+					-- 	desc = ' dotfiles',
+					-- 	group = 'Number',
+					-- 	action = 'Telescope dotfiles',
+					-- 	key = 'd',
+					-- },
+				},
+			},
 		})
 	end,
 	dependencies = { { "nvim-tree/nvim-web-devicons" } },
