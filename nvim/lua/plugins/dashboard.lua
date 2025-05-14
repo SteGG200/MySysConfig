8731f085e5e5fd9e06b6146e6c7e1198560389ac
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-05-14 07:51:45 +0700

    feat: add more shortcuts in dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -15,22 +15,25 @@ return {
 
 		require("dashboard").setup({
 			-- config
-			theme = 'hyper',
+			theme = "hyper",
 			hide = {
 				statusline = false,
 			},
 			config = {
 				header = vim.split(logo, "\n"),
 				shortcut = {
-					{ desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
 					{
-						icon = ' ',
-						icon_hl = '@variable',
-						desc = 'Files',
-						group = 'Label',
-						action = 'Telescope find_files',
-						key = 'f',
+						desc = " Open file",
+						group = "Label",
+						action = "Telescope find_files",
+						key = "f",
 					},
+					{
+						desc = " New file",
+						action = "enew",
+						key = "n",
+					}
 					-- {
 					-- 	desc = ' Apps',
 					-- 	group = 'DiagnosticHint',
