58884935fe7c8442e8ac3369ddd09e7dc232423e
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-06-30 17:57:50 +0700

    feat: setup packages installer and list all essential packages

--- scripts/arch/install.sh
+++ scripts/arch/install.sh
@@ -22,4 +22,48 @@ EOF
 echo "Updating system..."
 sudo pacman -Syu
 
+# Install yay AUR helper if not present
+if ! command -v yay &> /dev/null; then
+	echo "Not found yay AUR helper"
+	echo "Installing yay AUR helper..."
+	sudo pacman -S --needed git base-devel --no-comfirm
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	echo "Building yay..."
+	makepkg -si --noconfirm
+	cd ..
+	rm -rf yay
+else
+	echo "yay is already installed"
+fi
+
+# Import utility funtions
+source $PWD/scripts/arch/utils.sh
+
+# Import package list
+source $PWD/scripts/arch/packages.conf
+source $PWD/scripts/arch/aur_packages.conf
+
+
+# Install packages by category
+echo "Installing system utilities..."
+install_packages "${SYSTEM_UTILS[@]}"
+install_aur_packages "${AUR_SYSTEM_UTILS[@]}"
+
+echo "Installing hyprland utilities..."
+install_packages "${HYPRLAND_UTILS[@]}"
+install_aur_packages "${AUR_HYPRLAND_UTILS[@]}"
+
+echo "Installing development tools..."
+install_packages "${DEV_TOOLS[@]}"
+
+echo "Installing input method..."
+install_packages "${INPUT_METHOD[@]}"
+
+
+echo "Installing common applications..."
+install_packages "${COMMON_APPS[@]}"
+
+echo "Installing fonts..."
+install_packages "${FONTS[@]}"
 
--- scripts/arch/utils.sh
+++ scripts/arch/utils.sh
@@ -1,5 +1,44 @@
 #! /usr/bin/env bash
 
+# Function to check if a package is installed
 is_installed() {
-	pacman -Qi "$1" $> /dev/null
+	pacman -Qi "$1" &> /dev/null
+}
+
+# Function to check if a group package is installed
+is_group_installed() {
+	pacman -Qg "$1" &> /dev/null
+}
+
+# Function to install packages if not already installed
+install_packages() {
+	local packages=("$@")
+	local to_install=()
+
+	for pkg in "${packages[@]}"; do
+		if ! is_installed "$pkg" && ! is_group_installed "$pkg"; then
+			to_install+=("$pkg")
+		fi
+	done
+
+	if [ ${#to_install[@]} -ne 0 ]; then
+		echo "Packages to install: ${to_install[*]}"
+		sudo pacman -S --noconfirm "${to_install[@]}"
+	fi
+}
+
+install_aur_packages() {
+	local packages=("$@")
+	local to_install=()
+
+	for pkg in "${packages[@]}"; do
+		if ! is_installed "$pkg" && ! is_group_installed "$pkg"; then
+			to_install+=("$pkg")
+		fi
+	done
+
+	if [ ${#to_install[@]} -ne 0 ]; then
+		echo "AUR packages to install: ${to_install[*]}"
+		yay -S --noconfirm "${to_install[@]}"
+	fi
 }
