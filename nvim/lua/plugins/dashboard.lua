a5e11bcb2f9ffba79ade7f093ea0d11be3cf6567
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2026-01-05 11:42:40 +0700

    feat: change dasboard theme to doom and customize shortcut buttons

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -12,42 +12,63 @@ return {
 ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
 		]]
 
-		logo = string.rep("\n", 5) .. logo .. "\n"
+		logo = string.rep("\n", 3) .. logo .. "\n\n"
+
+		local customizedCenter = {
+			{
+				icon = "󰒲 ",
+				desc = "Open Lazy",
+				action = "Lazy",
+				key = "l",
+			},
+			{
+				icon = " ",
+				desc = "Open File Browser",
+				action = "Telescope file_browser",
+				key = "f",
+			},
+			{
+				icon = " ",
+				desc = "Find File",
+				action = function ()
+					require("telescope.builtin").find_files()
+				end,
+				key = "p",
+			},
+			{
+				icon = " ",
+				desc = "Create New File",
+				action = "enew",
+				key = "a",
+			},
+			{
+				icon = " ",
+				desc = "Quit",
+				action = "q",
+				key = "q"
+			},
+		}
+
+		for _, button in ipairs(customizedCenter) do
+			button.desc = button.desc .. string.rep(" ", 35 - #button.desc)
+			button.key_format = "  %s"
+		end
 
 		require("dashboard").setup({
 			-- config
-			theme = "hyper",
+			theme = "doom",
 			hide = {
 				statusline = false,
 			},
 			config = {
 				header = vim.split(logo, "\n"),
-				shortcut = {
-					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
-					{
-						desc = " Open file",
-						group = "Label",
-						action = "Telescope file_browser",
-						key = "f",
-					},
-					{
-						desc = " New file",
-						action = "enew",
-						key = "n",
-					}
-					-- {
-					-- 	desc = ' Apps',
-					-- 	group = 'DiagnosticHint',
-					-- 	action = 'Telescope app',
-					-- 	key = 'a',
-					-- },
-					-- {
-					-- 	desc = ' dotfiles',
-					-- 	group = 'Number',
-					-- 	action = 'Telescope dotfiles',
-					-- 	key = 'd',
-					-- },
-				},
+				center = customizedCenter,
+				footer = function()
+					local stats = require("lazy").stats()
+					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+					return { "", "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+				end,
+				vertical_center = true,
 			},
 		})
 	end,
